14b983b4cadb3fc0dc64ea73f49de9e3b6e60d95
Yann Régis-Gianas <yrg@pps.univ-paris-diderot.fr>
2015-10-01 12:17:35 +0200

    TP 1.

---
+++ flap/src/hopix/hopixAST.ml
@@ -0,0 +1,30 @@
+(** The abstract syntax tree for hopix programs. *)
+
+open Position
+
+type program = definition located list
+
+and definition =
+  (** A toplevel definition for a value. *)
+  | DefineValue of identifier located * expression located
+
+and expression =
+  (** A literal is a constant written "as is". *)
+  | Literal of literal
+  (** A variable identifies a value. *)
+  | Variable of identifier
+  (** A local definition [val p = e₁ ; e₂]. *)
+  | Define of identifier located * expression located * expression located
+  (** A function application [a b]. *)
+  | Apply of expression located * expression located
+  (** A conditional expression. *)
+  | IfThenElse of expression located * expression located * expression located
+
+and literal =
+  | LInt  of int
+
+and identifier =
+  | Id of string
+
+and t = program
+
