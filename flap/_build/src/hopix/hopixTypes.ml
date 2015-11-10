fb9ff603644744572f9659f04b92c8a9dd746669
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2015-11-10 17:48:28 +0100

    Le répertoire _build ainsi que l'éxecutable ne sont pas versionnés

--- flap/_build/src/hopix/hopixTypes.ml
+++
@@ -1,8 +0,0 @@
-open HopixAST
-
-(** Type checker error message producer. *)
-let error = Error.error "during type checking"
-
-type typing_environment = unit
-
-let initial_typing_environment () = ()
--- flap/_build/src/utilities/extPPrint.ml
+++
@@ -1,7 +0,0 @@
-(** This module extends the PPrint library. *)
-
-open PPrint
-
-let ( ++ ) x y = x ^^ break 1 ^^ y
-
-
