8785c013c4e45f3abfee39871154e2f4041a0d93
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-12-22 20:07:27 +0100

    _

--- flap/src/hopix/hopixInterpreter.ml
+++ flap/src/hopix/hopixInterpreter.ml
@@ -235,7 +235,7 @@ and expression position runtime = function
 				     | VBool(false) -> expression' runtime e2
 				     | _ -> failwith "ERROR -_- "))
   | Literal l ->
-    print_string("FLAP C'EST DE LA MERDE");literal (Position.value l)
+    failwith("FLAP C'EST DE LA MERDE");literal (Position.value l)
 
   | Variable x ->
     Environment.lookup (Position.value x) runtime.environment
