bd8d10c7a06f98ed097256d9f9a478b4bcadcb82
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2016-01-18 18:22:24 +0100

    Memory allocation

--- flap/src/hopix/memory.ml
+++ flap/src/hopix/memory.ml
@@ -1,12 +1,23 @@
 
 
-type address = int
+type address = HopixAST.label
 
-type 'v t = address * HopixAST.expression list
+type 'v t = (address * HopixAST.expression) list
 
-let fresh () = failwith "Students! This is your job!"
+let fresh () = [];;
+
 let read _ _ _ = failwith "Students! This is your job!"
 let read_block _ _ = failwith "Students! This is your job!"
 let write _ _ _ _ = failwith "Students! This is your job!"
-let allocate _ _ = failwith "Students! This is your job!"
+
+let allocate memory record = 
+	let rec malloc m r = 
+		match r with
+		| [] -> m
+		| (l,v)::q -> malloc ((l,v)::m) q
+    in
+    	malloc memory record
+;;
+
+	(*failwith "Students! This is your job!"*)
 
