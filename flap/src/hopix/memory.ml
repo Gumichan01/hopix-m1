a184b5b560c5b43d531c1ebbd525dfb087d53f53
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2016-01-22 18:55:45 +0100

    Modification de la mémoire + lecture d'un bloc

--- flap/src/hopix/memory.ml
+++ flap/src/hopix/memory.ml
@@ -1,21 +1,24 @@
 
 
-type address = HopixAST.label
+type address = int
 
-type 'v t = (address * HopixAST.ty) list
+type 'v t = (address * (HopixAST.label * HopixAST.ty) list) list
+
+let x = ref 1;;
 
 let fresh () = [];;
 
 let read _ _ _ = failwith "Students! This is your job!"
-let read_block _ _ = failwith "Students! This is your job!"
+let read_block (memory: 'v t) (adr : address) =
+  match memory with
+  | [] -> []
+  | _ -> List.assoc adr memory
+
+(*failwith "Students! This is your job!"*)
+
+
 let write _ _ _ _ = failwith "Students! This is your job!"
 
 let allocate (memory : 'v t) (record : (HopixAST.label * HopixAST.ty) list) =
-  let rec malloc m r =
-    match r with
-    | [] -> m
-    | (l,v)::q -> malloc ((l,v)::m) q
-  in
-  malloc memory record
+  let y = !x in x := !x + 1; (y,record)::memory
 ;;
-  (* failwith "Students! This is your job!";; *)
