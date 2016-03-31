54b3ac982ef6ac040d702ceb2fa23f4e307988e9
Yann Régis-Gianas <yrg@pps.univ-paris-diderot.fr>
2016-03-31 11:37:41 +0200

    HopixInterpreter: Fix inconsistent comment.

--- flap/src/hopix/hopixInterpreter.ml
+++ flap/src/hopix/hopixInterpreter.ml
@@ -6,7 +6,7 @@ open HopixAST
 let error positions msg =
   errorN "execution" positions msg
 
-(** Every expression of datix evaluates into a [value]. *)
+(** Every expression of hopix evaluates into a [value]. *)
 type 'e gvalue =
   | VInt          of Int32.t
   | VChar         of char
